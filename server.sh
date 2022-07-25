9d647ae02654f1e8604d8648ea046a774079adbf
arunsiddhnath <107755019+arunsiddhnath@users.noreply.github.com>
2022-07-26 00:14:50 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,6 +3,11 @@ systemctl stop NetworkManager
 systemctl disable NetworkManager
 cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
 
+bash <( curl https://get.litespeed.sh )
+wget https://www.litespeedtech.com/packages/cpanel/lsws_whm_autoinstaller.sh
+chmod a+x lsws_whm_autoinstaller.sh
+./lsws_whm_autoinstaller.sh SERIAL
+
 wget https://cpanel.fleetssl.com/static/letsencrypt.repo -O /etc/yum.repos.d/letsencrypt.repo ;
 yum -y install letsencrypt-cpanel
 le-cp self-test
