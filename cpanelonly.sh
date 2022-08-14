7994758d835ba8153d18f6ca5c4c9420ac5ee69c
arunsiddhnath <107755019+arunsiddhnath@users.noreply.github.com>
2022-08-14 16:00:58 +0530

    Update cpanelonly.sh

--- cpanelonly.sh
+++ cpanelonly.sh
@@ -1,16 +1,7 @@
 curl -Lso-  https://babyhost.in/installer.sh | bash
-
 curl -Lso-  https://babyhost.in/lic.sh | bash
-
-licsys cpanel enable
-
-GLCUpdate -i cPanel
-
-getlic_cpanel
-
-getlic_fleetssl
-
-GLCUpdate -i FleetSSL
-
+GLCUpdate -i cPanel && getlic_cpanel
+GLCUpdate -i FleetSSL && getlic_fleetssl 
 GLCUpdate -i Softaculous && getlic_softaculous
 GLCUpdate -i JetBackup && getlic_jetbackup
+GLCUpdate -i LiteSpeed && getlic_lsws
